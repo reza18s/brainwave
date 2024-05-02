8cb46b057f7a1b9540d4592cbf3cec7beb8159d4
reza 16t <swary2021@gmail.com>
2024-05-02 19:17:54 +0330

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Hero></Hero>
         <Benefits></Benefits>
         <Collaboration></Collaboration>
+        <Services></Services>
       </div>
       <ButtonGradient></ButtonGradient>
     </>
